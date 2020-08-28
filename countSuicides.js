5941fc676fa61de09780062e04058ef4a6284b2f
Lucas <lucas.tx.reis@gmail.com>
2020-08-28 17:37:37 -0300

    Refactoring for future self reliefw

--- countSuicides.js
+++ countSuicides.js
@@ -1,48 +1,49 @@
 function countSuicides(){
-
+	
+	// SVG params
 	var svg = d3.select("svg"),
 	margin = 200,
 	width = svg.attr("width") - margin,
 	height = svg.attr("height") - margin;
 
+	// Adjusting data to fit the SVG
 	var xScale = d3.scaleBand().range([0, width]).padding(0.4),
 		yScale = d3.scaleLinear().range([height, 0]);
 		
-		var g = svg.append("g")
+	var g = svg.append("g")
 		.attr("transform", "translate(" + 100 + "," + 100 + ")")
 		
+	// Loading data and building the bar chart	
+	d3.csv("https://raw.githubusercontent.com/lucas-t-reis/BioVis-IEEE-2020/master/dataset/question3.csv?token=ADE4HTVC6Y2PBDNBEHJAT227KKKQU").then(function(data) {
 		
-	d3.csv("/dataset/question3.csv").then(function(data) {
-		
-		var numSuicides = {}
+		var numSuicides = {};
 		
 		data.forEach(function(d){
 			if(!(d.KindredID in numSuicides))
-			numSuicides[d.KindredID] = 0;
+				numSuicides[d.KindredID] = 0;
 			numSuicides[d.KindredID]++
 		});
 			
 		var xdata = Object.keys(numSuicides)
 		var ydata = Object.values(numSuicides)
 
-
+		// Transformation real data -> pixels
 		xScale.domain(xdata.map(d => d));
 		yScale.domain([0, d3.max(ydata, d => d)]);
-
+		
+		// Placing the x,y axes
 		g.append("g")
 			.attr("transform", "translate(0," + height + ")")
 			.call(d3.axisBottom(xScale));
-
 		g.append("g")
-			.call(d3.axisLeft(yScale).tickFormat(function(d) { 
-				return d;
-			}).ticks(10))
+			.call(d3.axisLeft(yScale).tickFormat(d => d).ticks(10))
 
+		// Building rectangles
 		g.selectAll(".bar")
 			.data(ydata)
 			.enter().append("rect")
 			.attr("class", "bar")
-			.attr("x", (d, i) => (xScale(xdata[i]))) 
+			.attr("x", (d, i) => xScale(xdata[i])) 
 			.attr("y", d => yScale(d))
 			.attr("width", xScale.bandwidth())
 			.attr("height", d => height - yScale(d));
--- splitCSV.py
+++ splitCSV.py
@@ -1,59 +1,48 @@
 import csv
 
 pessoas = {}
+attributes = ['alcohol', 'psychosis', 'anxiety-non-trauma', 'somatic disorder', 'eating', 'bipolar spectrum illness', 'depression', 'interpersonal trauma', 'PD-Cluster C-anxiety', 'PD-Cluster B-emotional', 'PD', 'Impulse control disorder', 'obesity', 'cardiovascular', 'COPD', 'asthma', 'immune-autoimmune']
 
+# Getting each person and its attributes and grouping according to families
 with open('dataset/TenFamiliesAttributes.csv', newline='') as inputFile:
-	reader = csv.DictReader(inputFile)
-	for row in reader:
-		
-		#Initialize
-		pessoa = {}
-		
-		#Attributes
-		pessoa['personid'] = int(row['personid'])
-		pessoa['alcohol'] = row['alcohol'] == 'True'
-		pessoa['psychosis'] = row['psychosis'] == 'True'
-		pessoa['anxiety-non-trauma'] = row['anxiety-non-trauma'] == 'True'
-		pessoa['somatic disorder'] = row['somatic disorder'] == 'True'
-		pessoa['eating'] = row['eating'] == 'True'
-		pessoa['bipolar spectrum illness'] = row['bipolar spectrum illness'] == 'True'
-		pessoa['depression'] = row['depression'] == 'True'
-		pessoa['interpersonal trauma'] = row['interpersonal trauma'] == 'True'
-		pessoa['PD-Cluster C-anxiety'] = row['PD-Cluster C-anxiety'] == 'True'
-		pessoa['PD-Cluster B-emotional'] = row['PD-Cluster B-emotional'] == 'True'
-		pessoa['PD'] = row['PD'] == 'True'
-		pessoa['Impulse control disorder'] = row['Impulse control disorder'] == 'True'
-		pessoa['obesity'] = row['obesity'] == 'True'
-		pessoa['cardiovascular'] = row['cardiovascular'] == 'True'
-		pessoa['COPD'] = row['COPD'] == 'True'
-		pessoa['asthma'] = row['asthma'] == 'True'
-		pessoa['immune-autoimmune'] = row['immune-autoimmune'] == 'True'
-		
-		#Add to person list
-		pessoas[int(row['personid'])] = pessoa
+        reader = csv.DictReader(inputFile)
+        for row in reader:
+                
+                #Initialize
+                pessoa = {}
+                
+                #Attributes
+                personid = int(row['personid'])
+                pessoa['personid'] = personid 
+
+                for attribute in attributes:
+                    pessoa[attribute] = row[attribute] == 'True'
+                
+                #Add to person list
+                pessoas[personid] = pessoa
 
 with open('dataset/TenFamiliesStructure.csv', newline='') as inputFile:
-	reader = csv.DictReader(inputFile)
-	for row in reader:
-		
-		#Initialize
-		pessoa = {}
-		personid = int(row['personid'])
-		
-		#Ignoring non-suiciders
-		if personid not in pessoas.keys():
-			continue
+        reader = csv.DictReader(inputFile)
+        for row in reader:
+                
+                #Initialize
+                pessoa = {}
+                personid = int(row['personid'])
+                
+                #Ignoring non-suiciders
+                if personid not in pessoas.keys():
+                        continue
 
-		#Attributes
-		pessoas[personid]['KindredID'] = int(row['KindredID'])
-		pessoas[personid]['suicide'] = row['suicide'] == 'Y'
-		pessoas[personid]['sex'] = row['sex']
-		pessoas[personid]['Age'] = int(float(row['Age']))
+                #Attributes
+                pessoas[personid]['KindredID'] = int(row['KindredID'])
+                pessoas[personid]['suicide'] = row['suicide'] == 'Y'
+                pessoas[personid]['sex'] = row['sex']
+                pessoas[personid]['Age'] = int(float(row['Age']))
 
 with open('dataset/question3.csv', 'w', newline='') as outputFile:
-	fields = ['personid', 'KindredID', 'suicide', 'sex', 'Age', 'alcohol', 'psychosis', 'anxiety-non-trauma', 'somatic disorder', 'eating', 'bipolar spectrum illness', 'depression', 'interpersonal trauma', 'PD-Cluster C-anxiety', 'PD-Cluster B-emotional', 'PD', 'Impulse control disorder', 'obesity', 'cardiovascular', 'COPD', 'asthma', 'immune-autoimmune']		
-	writer = csv.DictWriter(outputFile, fieldnames=fields)
+        fields = ['personid', 'KindredID', 'suicide', 'sex', 'Age'] + attributes
+        writer = csv.DictWriter(outputFile, fieldnames=fields)
 
-	writer.writeheader()
-	for row in pessoas:
-		writer.writerow(pessoas[row])
+        writer.writeheader()
+        for row in pessoas:
+                writer.writerow(pessoas[row])
